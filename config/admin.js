5713960496f9136fcd054776c92618ee71d02b0d
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-12-07 18:32:47 +0100

    ok

--- config/admin.js
+++ config/admin.js
@@ -1,18 +1,38 @@
+// module.exports = ({ env }) => ({
+//   auth: {
+//     secret: env('ADMIN_JWT_SECRET')
+//   },
+//   // url: "/dashboard",
+//   apiToken: {
+//     salt: env('API_TOKEN_SALT')
+//   },
+//   transfer: {
+//     token: {
+//       salt: env('TRANSFER_TOKEN_SALT')
+//     }
+//   },
+//   flags: {
+//     nps: env.bool('FLAG_NPS', true),
+//     promoteEE: env.bool('FLAG_PROMOTE_EE', true)
+//   }
+// })
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET')
+    secret: env('ADMIN_JWT_SECRET', '3005f8e3851145c9e8e012f6e63bfdf2'),
   },
-  // url: "/dashboard",
+  url: "/dashboard",
   apiToken: {
-    salt: env('API_TOKEN_SALT')
+    salt: env('API_TOKEN_SALT'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT')
-    }
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true)
-  }
-})
+    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  },
+  // Disable SSL for local development
+  ssl: false,
+});
