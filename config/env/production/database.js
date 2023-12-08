4b3dcd42e89739f0dd202316b2d8b83efe940e22
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-12-08 12:16:19 +0100

    ok

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,42 @@
-const { parse } = require('pg-connection-string')
+// const { parse } = require('pg-connection-string')
+
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env('DATABASE_URL'))
+//   return {
+//     connection: {
+//       client: 'postgres',
+//       connection: {
+//         host,
+//         port,
+//         database,
+//         user,
+//         password
+//       },
+//       debug: false
+//     }
+//   }
+// }
+
+const { parse } = require('pg-connection-string');
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env('DATABASE_URL'))
+  const { host, port, database, user, password, ssl } = parse(env('DATABASE_URL'));
+
+  // Убедимся, что SSL включен
+  const connectionSettings = {
+    host,
+    port,
+    database,
+    user,
+    password,
+    ssl: ssl ? { rejectUnauthorized: false } : false, // Установим rejectUnauthorized в false, чтобы разрешить самоподписанные сертификаты
+  };
+
   return {
     connection: {
       client: 'postgres',
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password
-      },
-      debug: false
+      connection: connectionSettings,
+      debug: false,
     }
-  }
-}
+  };
+};
