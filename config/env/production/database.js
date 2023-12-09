c3de7f214a7201fccf9057258cf4e36bc3108706
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-12-09 12:44:26 +0100

    ssl

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,51 @@
-const { parse } = require('pg-connection-string')
+// const { parse } = require('pg-connection-string')
 
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password, ssl } = parse(env('DATABASE_URL'))
+//   return {
+//     connection: {
+//       client: 'postgres',
+//       connection: {
+//         host,
+//         port,
+//         database,
+//         user,
+//         password,
+//         ssl: false,
+//       },
+
+//       debug: false
+//     }
+//   }
+// }
+
+// ----------------------------------------
+
+// const { parse } = require('pg-connection-string')
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'strapi'),
+//       user: env('DATABASE_USERNAME', 'strapi'),
+//       password: env('DATABASE_PASSWORD', 'strapi'),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       ssl: {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//       },
+//     },
+//     debug: false,
+//   },
+// });
+
+const { parse } = require('pg-connection-string')
 module.exports = ({ env }) => {
-  const { host, port, database, user, password, ssl } = parse(env('DATABASE_URL'))
+  const { host, port, database, user, password, ssl } = parse(
+    env('DATABASE_URL')
+  )
   return {
     connection: {
       client: 'postgres',
@@ -10,11 +54,11 @@ module.exports = ({ env }) => {
         port,
         database,
         user,
-        password
+        password,
+        // ssl: env.bool('DATABASE_SSL', false)
+        ssl: env('DATABASE_SSL', false)
       },
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // Set to true in production
-      },
+
       debug: false
     }
   }
@@ -44,8 +88,6 @@ module.exports = ({ env }) => {
 //   };
 // };
 
-
-
 // const { parse } = require('pg-connection-string')
 
 // module.exports = ({ env }) => ({
@@ -65,3 +107,12 @@ module.exports = ({ env }) => {
 //     debug: false,
 //   },
 // });
+
+// # Database
+// DATABASE_CLIENT=mysql
+// DATABASE_HOST=127.0.0.1
+// DATABASE_PORT=3306
+// DATABASE_NAME=strapi
+// DATABASE_USERNAME=strapi
+// DATABASE_PASSWORD=strap1
+// DATABASE_SSL=false
